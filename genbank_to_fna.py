5de980e9c3d35b759b197d53f48511591347991c
Jin Choi <jinchoi721@gmail.com>
2016-11-21 10:46:18 -0600

    update

--- genbank_to_fna.py
+++ genbank_to_fna.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
 #This script generate nucloetide sequence from genbank in fasta form
+#gzip(gz) support
 #Usage: python genbank_to_fna.py genbankfile.gbk > output.fna
+#usage: python genbank_to_fna.py genbankfile.gbk.gz > output.fna
 
 import sys
+import gzip
 from Bio import SeqIO
 
 def main():
-    gb_file = sys.argv[1]
-    for gb_record in SeqIO.parse(open(gb_file,'r'),"genbank") :
+    if sys.argv[1][-2:] == 'gz':
+        gb_file = gzip.open(sys.argv[1],'rb')
+    else:
+        gb_file = open(sys.argv[1],'r')
+
+    for gb_record in SeqIO.parse(gb_file,"genbank") :
         genome_name = gb_record.name
         for feat in gb_record.features:
             if feat.type == "CDS":
