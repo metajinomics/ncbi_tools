45a8e78830b630e1f9098598c5f0af35a2bd17e7
Jin Choi <jinchoi721@gmail.com>
2017-04-18 14:22:29 -0500

    update

--- split_file_paired.py
+++ split_file_paired.py
@@ -14,10 +14,10 @@ for n, line in enumerate(open(sys.argv[1],'r')):
     if n % 4 == 0:
         spl = line.strip().split(' ')
         if spl[len(spl)-1][0] == '1':
-            r1.write("@"+spl[1]+' '+spl[3])
+            r1.write("@"+spl[1]+' '+spl[3]+'\n')
             r = 1
         else:
-            r2.write("@"+spl[1]+' '+spl[3])
+            r2.write("@"+spl[1]+' '+spl[3]+'\n')
             r = 2
     else:
         if r == 1:
