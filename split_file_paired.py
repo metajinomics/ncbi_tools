ae2f25410f3625eac6e49ef57fac8091d72943f1
Jin Choi <jinchoi721@gmail.com>
2017-04-19 11:51:38 -0500

    update

--- split_file_paired.py
+++ split_file_paired.py
@@ -8,19 +8,41 @@ import sys
 he = sys.argv[1].split('.')[0]
 r1 = open(he+"_1.fastq",'w')
 r2 = open(he+"_2.fastq",'w')
-
+s1 = open(he+"_single.fastq",'w')
 r = 1
+
+flag = 0
+save1 = []
+save2 = []
+ini = 0
 for n, line in enumerate(open(sys.argv[1],'r')):
     if n % 4 == 0:
         spl = line.strip().split(' ')
         if spl[len(spl)-1][0] == '1':
-            r1.write("@"+spl[1]+' '+spl[3]+'\n')
+            if ini == 0:
+                ini = 1
+            else:
+                if len(save2) == 0:
+                    for x in save1:
+                        s1.write(x)
+                        save1 = []
+                else:
+                    for x in save1:
+                        r1.write(x)
+                        save1 = []
+                    for x in save2:
+                        r2.write(x)
+                        save2 = []
+
+            save1.append("@"+spl[1]+' '+spl[3]+'\n')
             r = 1
+
         else:
-            r2.write("@"+spl[1]+' '+spl[3]+'\n')
+            save1.append("@"+spl[1]+' '+spl[3]+'\n')
             r = 2
+
     else:
         if r == 1:
-            r1.write(line)
+            save1.append(line)
         else:
-            r2.write(line)
+            save2.append(line)
